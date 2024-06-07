834f55daa0417909014877f34ab85065b9998359
Caio Vinicius Carvalho de Cerqueira <caio_vinicius89@hotmail.com>
2024-06-07 09:23:50 -0300

    Initial commit

---
+++ BenchmarkDotNet/BenchMarkTest.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDotNetTest
+{
+    public class BenchMarkTest
+    {
+        [Benchmark]
+        public int[] BubbleSort()
+        {
+            int[] listaNumeros = { 64, 34, 25, 12, 22, 11, 90 };
+
+            int n = listaNumeros.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (listaNumeros[j] > listaNumeros[j + 1])
+                    {
+                        int temp = listaNumeros[j];
+                        listaNumeros[j] = listaNumeros[j + 1];
+                        listaNumeros[j + 1] = temp;
+                    }
+                }
+            }
+            return listaNumeros;
+        }
+
+        [Benchmark]
+        public int[] InsertionSort()
+        {
+            int[] listaNumeros = { 64, 34, 25, 12, 22, 11, 90 };
+
+            int n = listaNumeros.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = listaNumeros[i];
+                int j = i - 1;
+
+                while (j >= 0 && listaNumeros[j] > key)
+                {
+                    listaNumeros[j + 1] = listaNumeros[j];
+                    j = j - 1;
+                }
+                listaNumeros[j + 1] = key;
+            }
+
+            return listaNumeros;
+        }
+    }
+}
---
+++ BenchmarkDotNet/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using BenchmarkDotNet.Running;
+using BenchmarkDotNetTest;
+
+var summary = BenchmarkRunner.Run<BenchMarkTest>();
